0060da1189e4dc4c432b3efe597166854fe8c308
Mak Arnautovic <mak@tablexi.com>
2020-05-30 16:25:49 -0500

    Update path for infect script

--- scripts/infect.sh
+++ scripts/infect.sh
@@ -1,4 +1,4 @@
 echo "Provide the username and repo name portion of the URL (e.g. '/arnamak/infect.git')"
 read plugin
-cd ~/.vim/bundle && \
+cd ~/.config/nvim/bundle && \
     git clone git://github.com"$(echo $plugin)"
